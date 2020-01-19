8ae7572eae7e502a3cf29709e39528cecb59c9bd
Shri Kant Lal <46701610+lalshrikant12@users.noreply.github.com>
2020-01-19 22:23:42 +0530

    Create javaloops1.java

---
+++ javaloops1.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution
+{
+public static void main(String arg[])
+{
+Scanner sc=new Scanner(System.in);
+  int n = sc.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", n, i, n*i);
+
+}
+}
+}
